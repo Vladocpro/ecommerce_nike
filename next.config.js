94082b0be6640b521010931f7f8dcf62007294cd
vladocpro <infernohs9@gmail.com>
2023-06-27 18:56:06 +0100

    Test for deploy

--- next.config.js
+++ next.config.js
@@ -9,12 +9,7 @@ const nextConfig = {
       'lh3.googleusercontent.com',
       'static.nike.com'
     ]
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  }
 }
 
 module.exports = nextConfig
